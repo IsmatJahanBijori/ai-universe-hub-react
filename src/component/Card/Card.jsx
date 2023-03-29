4f983cffb50c4b13304bf13f778b9aa54c94da88
IsmatJahanBijori <ismatjahanbijori@gmail.com>
2023-03-29 15:27:58 +0600

    integration hoy na

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '../Button/Button';
 import Modal from '../Modal/Modal';
 import SingleCard from '../SingleCard/SingleCard';
@@ -7,26 +7,36 @@ import SingleCard from '../SingleCard/SingleCard';
 
 const Card = () => {
     const [card, setCard] = useState([]);
-    const [showAll, setShowAll]=useState(false)
-    const [uniqueId, setUniqueId]=useState(null)
-console.log(uniqueId)
-    const handleShowAll=()=>{
+    const [showAll, setShowAll] = useState(false)
+    const [uniqueId, setUniqueId] = useState(null)
+    const [singleData, setSingleData] = useState({})
+
+    // console.log(uniqueId)
+    const handleShowAll = () => {
         setShowAll(true)
     }
+
     useEffect(() => {
         fetch('https://openapi.programming-hero.com/api/ai/tools')
-        .then(res=>res.json())
-        .then(data=>setCard(data.data.tools))
+            .then(res => res.json())
+            .then(data => setCard(data.data.tools))
     }, []);
+
+    useEffect(() => {
+        fetch(`https://openapi.programming-hero.com/api/ai/tool/${uniqueId}`)
+            .then(res => res.json())
+            .then(data => setSingleData(data.data))
+    }, [uniqueId]);
+    // console.log(singleData)
     // console.log(data.data.tools)
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
             {
-                card.slice(0, showAll ? 12 : 6).map((singleCard)=><SingleCard singleCard={singleCard} key={singleCard.id} setUniqueId={setUniqueId}></SingleCard>)
+                card.slice(0, showAll ? 12 : 6).map((singleCard) => <SingleCard singleCard={singleCard} key={singleCard.id} setUniqueId={setUniqueId}></SingleCard>)
             }
             {(!showAll) && <span onClick={handleShowAll}>
-            <Button>See More</Button></span>}
-            <Modal></Modal>
+                <Button>See More</Button></span>}
+            <Modal singleData={singleData}></Modal>
         </div>
     );
 };
--- src/component/Modal/Modal.jsx
+++ src/component/Modal/Modal.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 
-const Modal = () => {
+const Modal = (props) => {
+    const {image_link, description, integrations, features } = props.singleData
+    console.log(integrations)
     return (
         <div>
             <input type="checkbox" id="my-modal-5" className="modal-toggle" />
             <div className="modal">
                 <div className="modal-box w-11/12 max-w-5xl">
-                    <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
-                    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                    <div className="card lg:card-side bg-base-100">
+                        <div className="card-body">
+                            <h2 className="card-title">{description ? description : "Not found"}</h2>
+
+                            <div className='flex justify-between'>
+                            <div>
+                            <h3 className='text-xl font-semibold'>Features</h3>
+                            {
+                                Object.values(features || {}).map(feature=> <li>{feature.feature_name}</li>)
+                            }
+                            </div>
+                            <div>
+                            <h3 className='text-xl font-semibold'>Integration</h3>
+                            </div>
+                            </div>
+                        </div>
+                        <div>
+                        <figure className='w-full'><img className='w-full h-96' src={image_link && image_link[0]} alt="Album" /></figure></div>
+                    </div>
                     <div className="modal-action">
                         <label htmlFor="my-modal-5" className="btn">Yay!</label>
                     </div>
